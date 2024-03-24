f945f4702b37fc6791767df2aff0fd7a97707d1a
DMould123 <95115610+DMould123@users.noreply.github.com>
2024-03-24 22:46:18 +0100

    Implement form for adding new transfers with validation. Imported toasitfy to use for notifications

--- src/components/AddTransfer.vue
+++ src/components/AddTransfer.vue
@@ -1,17 +1,46 @@
 <template>
   <h3>Add new transfer</h3>
-  <form id="form">
+  <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="player">Player</label>
-      <input type="text" id="player" placeholder="Enter player..." />
+      <input
+        type="text"
+        id="player"
+        v-model="player"
+        placeholder="Enter player..."
+      />
     </div>
     <div class="form-control">
       <label for="amount"
         >Transfer Fee <br />
         (outgoing - transfer fee, incoming - transfer fee)</label
       >
-      <input type="number" id="amount" placeholder="Enter amount..." />
+      <input
+        type="number"
+        id="amount"
+        v-model="amount"
+        placeholder="Enter amount..."
+      />
     </div>
     <button class="btn">Add transfer</button>
   </form>
 </template>
+
+<script setup>
+  import { ref } from 'vue'
+  import { useToast } from 'vue-toastification'
+
+  const player = ref('')
+  const amount = ref('')
+  const toast = useToast()
+
+  const onSubmit = () => {
+    if (!player.value || !amount.value) {
+      toast.error('Please complete both fields')
+      return
+    }
+    console.log(player.value, amount.value)
+    player.value = ''
+    amount.value = ''
+  }
+</script>
